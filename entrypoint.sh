83abce390ead25959edf1a0e47d6225ee1a20e0b
andrew <andrew@textile.io>
2020-02-14 19:20:18 -0700

    script: fix with chmod
    Signed-off-by: andrew <andrew@textile.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,8 @@ echo "token: $3" > $HOME/.textile/auth.yml
 wget "https://github.com/textileio/textile/releases/download/v0.0.2/textile_v0.0.2_linux-amd64.tar.gz"
 tar -xf textile_v0.0.2_linux-amd64.tar.gz
 echo $(ls ./)
-./install
+chmod +x textile
 
-textile bucket push $2 $1
+./textile bucket push $2 $1
 
 echo ::set-output name=cid::INCOMPLETE
