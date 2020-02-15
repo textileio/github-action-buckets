0a552a49bbbe88ddbed8c41dcb1fa4ccb28ed3d3
andrew <andrew@textile.io>
2020-02-14 22:12:21 -0700

    script: fix textile install
    Signed-off-by: andrew <andrew@textile.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,9 @@ echo "token: $3" > $HOME/.textile/auth.yml
 
 wget https://github.com/textileio/textile/releases/download/v0.0.2/textile_v0.0.2_linux-amd64.tar.gz
 tar -xvf textile_v0.0.2_linux-amd64.tar.gz
+chmod +x textile
 ./textile bucket push $2 $1
+./install
+textile bucket push $2 $1
 
 echo ::set-output name=cid::INCOMPLETE
