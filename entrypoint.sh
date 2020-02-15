160c04769d3cb24c42ecec2a45110ad6e42a34c7
andrew <andrew@textile.io>
2020-02-14 21:01:52 -0700

    script: fix textile install
    Signed-off-by: andrew <andrew@textile.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,10 @@
 
 #!/bin/sh
 set -e
-set -o pipefail
 
-main() {
-  echo "Updating Bucket $1 from $2"
-  time=$(date)
-  mkdir $HOME/.textile
-  echo "token: $3" > $HOME/.textile/auth.yml
-  ./textile bucket push $2 $1
-  echo ::set-output name=cid::INCOMPLETE
-}
-main
+echo "Updating Bucket $1 from $2"
+time=$(date)
+mkdir $HOME/.textile
+echo "token: $3" > $HOME/.textile/auth.yml
+./textile bucket push $2 $1
+echo ::set-output name=cid::INCOMPLETE
