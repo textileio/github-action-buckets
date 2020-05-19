747db1b327677de2f84949cb680f32e7eb7a351e
andrew <andrew@textile.io>
2020-05-18 18:26:56 -0700

    fix(tests): run on push
    Signed-off-by: andrew <andrew@textile.io>

--- dist/index.js
+++ dist/index.js
@@ -9184,7 +9184,7 @@ function run() {
                 // path = path === '' ? '.' : path
                 if (true) {
                     const files = yield globDir(pattern, {
-                        cwd: './',
+                        cwd: '/home/runner/work/',
                         nodir: true
                     });
                     core.setFailed(`No files found: ${files.join(', ')}`);
